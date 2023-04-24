1b40a974229b4c36539a87ca6ef6f58721f62140
Alina Drahos <alinchen1066@outlook.com>
2023-04-23 21:07:23 -0500

    Code added to Put and Get Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
+import Timeline from '@mui/lab/Timeline'
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 
 interface ToDoItem {
@@ -9,29 +15,64 @@ interface ToDoItem {
   isComplete: boolean
 }
 
+interface ButtonProps {
+  id: number
+}
+
+var dataObject: ToDoItem ={
+  id: 0,
+  name: "Feed Dracula",
+  isComplete : false
+} 
+
+function LeftPositionedTimeline(props: ButtonProps) {
+  return (
+    <TimelineItem>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>{props.id}</TimelineContent>
+    </TimelineItem>
+  );
+}
+
 function App() {
   const [data, setData] = useState([] as ToDoItem[]);
+  const [update,setUpdate] = useState(false);
 
   useEffect(() => {
     GetValues()
-  }, []);
+  }, [update]);
 
   async function GetValues() {
-    const dataFetched = await fetch("https://localhost:7129/api/TodoItems");
+    const dataFetched  = await fetch("https://localhost:7129/api/TodoItems");
     const dataToGet = await dataFetched.json() as ToDoItem[];
-
     setData(dataToGet);
   }
 
+  async function postValues(item:ToDoItem){
+    const response = await fetch("https://localhost:7129/api/TodoItems", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+    setUpdate(true);
+  }
+
   return (
+
     <>
-      {
-        data.map((item) => {
-          return (
-            <div>{item.id}</div>
-          )
-        })
-      }
+      <Timeline position="left">
+        {
+          data.map((item) => <div>{<LeftPositionedTimeline id={item.id} />}</div>)
+        }
+      </Timeline>
+      <button onClick={()=>postValues(dataObject)}>
+        Click me
+      </button>
     </>
   )
 }
