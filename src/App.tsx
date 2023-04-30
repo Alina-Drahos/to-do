f1110bf50217ec7dbe5b6babf13c5dd80a91dd48
Alina Drahos <alinchen1066@outlook.com>
2023-04-30 15:25:34 -0500

    Chaneges to display

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { CSSProperties, useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
 import Timeline from '@mui/lab/Timeline'
@@ -16,48 +16,67 @@ interface ToDoItem {
   isComplete: boolean
 }
 
-var dataObject: ToDoItem ={
+var dataObject: ToDoItem = {
   id: 0,
   name: "Feed Dracula",
-  isComplete : false
-} 
+  isComplete: false
+}
 
-function LeftPositionedTimeline(props: ToDoItem) { 
+function LeftPositionedTimeline(props: ToDoItem) {
   const [checked, setChecked] = useState(props.isComplete)
+  const [disabled, setDisabled] = useState(false);
+  const [style, setStyle] = useState<CSSProperties>();
 
-  async function PutValues(){
+  useEffect(() => {
+    if (checked) {
+      setStyle({ textDecorationLine: 'line-through', textDecorationStyle: 'solid' })
+    }
+    else {
+      setStyle(undefined);
+    }
+  }, [checked]);
+
+  async function PutValues() {
+    const originalValue = checked;
+    setDisabled(true);
     //Change state and create a new ToDoItem
-      setChecked(!checked);
+    setChecked(!originalValue);
 
-      var updatedItem = {
-        ...props,
-        isComplete : !checked
-      }
+    var updatedItem = {
+      ...props,
+      isComplete: !originalValue
+    }
 
-    const response = await fetch( `https://localhost:7129/api/TodoItems/${props.id}`,{
-      method:"PUT",
-      headers:{
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedItem),
-    })
+    try {
+      const response = await fetch(`https://localhost:7129/api/TodoItems/${props.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedItem),
+      })
+    }
+    catch {
+      setChecked(originalValue);
+    }
+    setDisabled(false);
   }
   return (
     <>
-    <TimelineItem>
-      <TimelineSeparator>
-        <TimelineDot />
-        <TimelineConnector />
-      </TimelineSeparator>
-      <TimelineContent>{props.name}</TimelineContent>
-      <Checkbox {...TimelineContent} checked={checked} color="success" onClick={()=> PutValues()} />
-    </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent style={style}>{props.name}</TimelineContent>
+        <Checkbox {...TimelineContent} checked={checked} disabled={disabled} color="success" onChange={() => PutValues()} />
+      </TimelineItem>
     </>
   );
 }
 
 function App() {
-  const[idData,setIdData] = useState(1);
+  const [idData, setIdData] = useState(1);
   const [data, setData] = useState([] as ToDoItem[]);
 
   useEffect(() => {
@@ -65,13 +84,13 @@ function App() {
   }, []);
 
   async function GetValues() {
-    const dataFetched  = await fetch("https://localhost:7129/api/TodoItems");
+    const dataFetched = await fetch("https://localhost:7129/api/TodoItems");
     const dataToGet = await dataFetched.json() as ToDoItem[];
     setData(dataToGet);
     setIdData(dataToGet[0].id);
   }
 
-  async function postValues(item:ToDoItem){
+  async function postValues(item: ToDoItem) {
     const response = await fetch("https://localhost:7129/api/TodoItems", {
       method: "POST", // or 'PUT'
       headers: {
@@ -82,8 +101,8 @@ function App() {
     GetValues();
   }
 
-  async function deleteValues(id:number){
-    const response = await fetch(`https://localhost:7129/api/TodoItems/${id}`,{
+  async function deleteValues(id: number) {
+    const response = await fetch(`https://localhost:7129/api/TodoItems/${id}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -97,13 +116,13 @@ function App() {
     <>
       <Timeline position="left">
         {
-          data.map((item) => <div>{<LeftPositionedTimeline name={item.name} isComplete={item.isComplete} id={item.id}/>}</div>)
+          data.map((item) => <div>{<LeftPositionedTimeline name={item.name} isComplete={item.isComplete} id={item.id} />}</div>)
         }
       </Timeline>
-      <button onClick={()=>postValues(dataObject)}>
+      <button onClick={() => postValues(dataObject)}>
         Click me
       </button>
-      <button onClick={()=> deleteValues(idData)}>I will delete something {idData}</button>
+      <button onClick={() => deleteValues(idData)}>I will delete something {idData}</button>
     </>
   )
 }
