0c58f3a1b454db23cdaf2573beedfdd6d303a25c
Alina Drahos <alinchen1066@outlook.com>
2023-04-30 14:30:19 -0500

    Added ability to modify value via checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import { Checkbox } from '@mui/material';
 
 
 interface ToDoItem {
@@ -15,26 +16,43 @@ interface ToDoItem {
   isComplete: boolean
 }
 
-interface ButtonProps {
-  id: number
-}
-
 var dataObject: ToDoItem ={
   id: 0,
   name: "Feed Dracula",
   isComplete : false
 } 
 
-function LeftPositionedTimeline(props: ButtonProps) {
- 
+function LeftPositionedTimeline(props: ToDoItem) { 
+  const [checked, setChecked] = useState(props.isComplete)
+
+  async function PutValues(){
+    //Change state and create a new ToDoItem
+      setChecked(!checked);
+
+      var updatedItem = {
+        ...props,
+        isComplete : !checked
+      }
+
+    const response = await fetch( `https://localhost:7129/api/TodoItems/${props.id}`,{
+      method:"PUT",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedItem),
+    })
+  }
   return (
+    <>
     <TimelineItem>
       <TimelineSeparator>
         <TimelineDot />
         <TimelineConnector />
       </TimelineSeparator>
-      <TimelineContent>{props.id}</TimelineContent>
+      <TimelineContent>{props.name}</TimelineContent>
+      <Checkbox {...TimelineContent} checked={checked} color="success" onClick={()=> PutValues()} />
     </TimelineItem>
+    </>
   );
 }
 
@@ -50,7 +68,7 @@ function App() {
     const dataFetched  = await fetch("https://localhost:7129/api/TodoItems");
     const dataToGet = await dataFetched.json() as ToDoItem[];
     setData(dataToGet);
-    setIdData(dataToGet[1].id);
+    setIdData(dataToGet[0].id);
   }
 
   async function postValues(item:ToDoItem){
@@ -79,7 +97,7 @@ function App() {
     <>
       <Timeline position="left">
         {
-          data.map((item) => <div>{<LeftPositionedTimeline id={item.id}/>}</div>)
+          data.map((item) => <div>{<LeftPositionedTimeline name={item.name} isComplete={item.isComplete} id={item.id}/>}</div>)
         }
       </Timeline>
       <button onClick={()=>postValues(dataObject)}>
